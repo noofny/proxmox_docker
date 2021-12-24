f97befc79f5969a2d20403706c07a6f0df55e17a
noofny <noofny@gmail.com>
2021-12-24 14:51:37 +1100

    Mount points.

--- setup_docker.sh
+++ setup_docker.sh
@@ -36,7 +36,7 @@ systemct1 enable docker
 
 
 echo "Installing Portainer..."
-mkdir /media/portainer
+mkdir /mnt/portainer  # mp0: /mnt/portainer,mp=/mnt/portainer,backup=0
 docker run \
     -d \
     -p 8000:8000 -p 9000:9000 \
